f3bed58f3d1d0a4f7b4e6d20c05c5240866ac8f2
IndhurekhaS <42089681+IndhurekhaS@users.noreply.github.com>
2019-05-02 15:40:53 +0530

    Create odd or even.java

---
+++ odd or even.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class hour
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int result=n+m;
+        if(result%2==0)
+        System.out.println("even");
+        else
+        System.out.println("odd");
+    }
+}
