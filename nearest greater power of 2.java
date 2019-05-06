d5dd96444a2d0e592b1a3790d10abd8482a1acc5
IndhurekhaS <42089681+IndhurekhaS@users.noreply.github.com>
2019-05-06 16:16:46 +0530

    Create nearest greater power of 2.java

---
+++ nearest greater power of 2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class power
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a=n*2;
+        System.out.println(a);
+    }
+}
